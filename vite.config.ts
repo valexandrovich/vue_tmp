e7b5ac2a87779a67d42cf50a1283162f7aa62e74
valexandrovich <beloenko23@gmail.com>
2025-11-23 00:28:40 +0100

    loadEnv 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd()) 
-  return defineConfig({
+export default defineConfig(({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  console.log('VITE_BASE_URL:', env.VITE_BASE_URL)
+
+  return {
     plugins: [vue()],
-    base: env.VITE_BASE_URL || '/tmp2', 
-  })
-}
+    base: env.VITE_BASE_URL || '/tmp/',
+  }
+})
